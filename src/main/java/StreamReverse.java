6fe934ef4f84692373c74a9f80797d24a5e9002b
CHANDRA SHEKHAR GOKA <chandrashekhar.goka@gmail.com>
2020-04-06 10:35:11 +0530

    updated with int array

--- src/main/java/StreamReverse.java
+++ src/main/java/StreamReverse.java
@@ -6,9 +6,9 @@ import java.util.stream.IntStream;
 public class StreamReverse {
     public static void main(String[] args) {
         deQueue();
-        reverseRange(1,10);
+        reverseRange(1, 10);
+        reverseIntArray(new int[]{1, 2, 3, 4, 5});
     }
-
     public static void deQueue(){
         List<Integer> list = Arrays.asList(1, 2, 3, 4);
         list.stream()
@@ -19,6 +19,11 @@ public class StreamReverse {
     public static void reverseRange(int from, int to){
         IntStream.range(from, to)
                 .map(i -> to - i + from - 1)
+                .forEach(System.out::println);
+    }
+    public static void reverseIntArray(int[] a){
+        IntStream.range(0, a.length)
+                .map(i -> a[a.length - 1 - i])
                 .forEach(System.out::println);
     }
 }
